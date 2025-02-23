a2d075ac3e2c7dddca628bd6e3f8f1b21c16d794
Anh Trí <Anhtri22303@gmail.com>
2025-02-23 20:50:49 +0700

    .

---
+++ client/src/features/purchase/purchaseSlice.js
@@ -0,0 +1,19 @@
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  purchases: [],
+};
+
+const purchaseSlice = createSlice({
+  name: "purchase",
+  initialState,
+  reducers: {
+    setPurchases: (state, action) => {
+      state.purchases = action.payload;
+    },
+  },
+});
+
+export const { setPurchases } = purchaseSlice.actions;
+export default purchaseSlice.reducer;
